3c309ce8730e61a212143c009f559914afb9ba80
Abderrahmane-haouaria <109762566+Abderrahmane-haouaria@users.noreply.github.com>
2022-07-27 18:50:47 -0400

    Added some features in 'Watchlist' page.

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function() {
 
+    // Get hold of the 'button' elements for each page.
     watchlistBtn = document.querySelector("#watchlist-btn");
     aboutBtn = document.querySelector("#about-btn");
     registerBtn = document.querySelector("#register-btn");
 
+    // The default is to show only 'watchlist'.
+    document.querySelector("#about").style.display = "none";
+    document.querySelector("#register").style.display = "none";
+
+    // For each button, once it's clicked, show the corresponding 'div'
+    // and hide the other two.
     watchlistBtn.addEventListener("click", () => {
 
         document.querySelector("#watchlist").style.display = "block";
-        document.querySelector("#about").style.display = "hidden";
-        document.querySelector("#register").style.display = "hidden";
+        document.querySelector("#about").style.display = "none";
+        document.querySelector("#register").style.display = "none";
     });
 
     aboutBtn.addEventListener("click", () => {
         
-        document.querySelector("#watchlist").style.display = "hidden";
+        document.querySelector("#watchlist").style.display = "none";
         document.querySelector("#about").style.display = "block";
-        document.querySelector("#register").style.display = "hidden";
+        document.querySelector("#register").style.display = "none";
     });
 
     registerBtn.addEventListener("click", () => {
         
-        document.querySelector("#watchlist").style.display = "hidden";
-        document.querySelector("#about").style.display = "hidden";
+        document.querySelector("#watchlist").style.display = "none";
+        document.querySelector("#about").style.display = "none";
         document.querySelector("#register").style.display = "block";
     });
 
